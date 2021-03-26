caf6b8b3824c77dfb09bd4d4084a4b10831822cc
nikhilramgiri <63799117+nikhilramgiri@users.noreply.github.com>
2021-03-26 14:39:24 +0530

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// alert("Hello Nikhil")
+let age =24
+let year = 2021
+console.log(age,year)
+
+let name1 = 'My name is nikhil';
+console.log(name1[0]);
+name1 = 'Nikhil';
+let last = 'Ramgiri';
+let fullname= name1 + ' ' + last;
+console.log(fullname)
+console.log(fullname[0])
